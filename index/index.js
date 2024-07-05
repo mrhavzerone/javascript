38d4edbb36d25f834081cb60a82f388236db6ade
mrhavzerone <mrhavzerone@proton.me>
2024-07-05 10:46:09 +0300

    basic course ended

--- index/index.js
+++ index/index.js
@@ -1,2 +1,8 @@
 console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
 alert("hello");
